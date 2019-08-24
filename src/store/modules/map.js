a5dd06623fbb7a6c5043e1a706cd7d723be9c975
masahiro hosoya <hosoya1985@gmail.com>
2019-08-25 02:22:06 +0900

    update store

--- src/store/modules/map.js
+++ src/store/modules/map.js
@@ -3,7 +3,16 @@ const map = {
   state: {
     lat: null,
     lng: null,
-    zoom: null
+    zoom: null,
+    /**
+     * {
+     *   ne_lat: 表示されている地図の緯度が最も高い位置の緯度の値
+     *   sw_lat: 表示されている地図の緯度が最も低い位置の緯度の値
+     *   ne_lng: 表示されている地図の経度が最も東の位置の経度の値
+     *   sw_lng: 表示されている地図の経度が最も西の位置の経度の値
+     * }
+     */
+    bounds: null
   },
   mutations: {
     latLng(state, payload) {
@@ -12,6 +21,9 @@ const map = {
     },
     zoom(state, payload) {
       state.zoom = payload.zoom
+    },
+    bounds(state, payload) {
+      state.bounds = payload
     }
   },
   actions: {
@@ -20,10 +32,14 @@ const map = {
     },
     setZoom({ commit }, { zoom }) {
       commit('zoom', { zoom })
+    },
+    setbounds({ commit }, bounds) {
+      commit('bounds', bounds)
     }
   },
   getters: {
-    latLngZoom: state => ({ lat: state.lat, lng: state.lng, zoom: state.zoom })
+    latLngZoom: state => ({ lat: state.lat, lng: state.lng, zoom: state.zoom }),
+    displayBounds: state => state.bounds
   }
 }
 
