c97c9f5edfac677bb14d40ccb23c458df0427f3a
masahiro hosoya <hosoya1985@gmail.com>
2019-08-25 02:21:54 +0900

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import routes from './route'
+import { routes } from './route'
 
 Vue.use(Router)
 
--- src/router/route.js
+++ src/router/route.js
@@ -1,17 +1,17 @@
 import Home from '../components/pages/Home.vue'
-export default [
+export const routes = [
   {
     path: '/',
     name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/map',
+    name: 'map',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../components/pages/About.vue')
+      import(/* webpackChunkName: "map" */ '../components/pages/Map.vue')
   }
 ]
